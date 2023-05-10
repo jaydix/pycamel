17ebaaf760dd36c2a3f8929dc1754bcef4b4d193
8bitjake <75950907+8bitjake@users.noreply.github.com>
2023-05-10 20:31:55 +0000

    add support for files of other names

--- .build/index.js
+++ .build/index.js
@@ -1,60 +1,91 @@
 "use strict";
-var import_fs = require("fs");
-const orig = (0, import_fs.readFileSync)("test.pyc", "utf8");
-var lines = orig.replaceAll("function", "def").replaceAll("constructor(", "__init__(self,").replaceAll("this", "self").split("\n");
-var converted = "";
-String.prototype.isUpperCase = function() {
-  if (!/^[a-zA-Z]+$/.test(this.toString()))
-    return false;
-  return this.toString() === this.toUpperCase();
-};
-String.prototype.isLowerCase = function() {
-  if (!/^[a-zA-Z]+$/.test(this.toString()))
-    return false;
-  return this.toString() === this.toLowerCase();
-};
-String.prototype.isModifiable = function() {
-  return /^[a-zA-Z]+$/.test(this.toString());
-};
-var indentLevel = 0;
-lines.forEach((l, i) => {
-  var stringSoFar = "";
-  var prev = "";
-  l.split("").forEach((c, i2) => {
-    const prev2 = l.split("")[i2 - 1];
-    if (/^\s+$/.test(c)) {
-      if (/^\s+$/.test(c + prev2))
-        return;
-    }
-    if (c == "}") {
-      indentLevel--;
-      return;
-    }
-    if (c == "{" && i2 == l.length - 1) {
-      indentLevel++;
-      stringSoFar += ":";
-      return;
-    }
-    if (i2 == 0)
-      stringSoFar = indent(indentLevel) + stringSoFar;
-    if (c.isUpperCase()) {
-      c = "_" + c.toLowerCase();
-    }
-    if (c !== ";")
-      stringSoFar += c;
-    if (stringSoFar == "var ")
-      stringSoFar = "";
-  });
-  if (!/^\s+$/.test(stringSoFar))
-    converted += stringSoFar + "\n";
-});
-console.log(converted);
-(0, import_fs.writeFileSync)("test_converted.py", converted);
-function indent(lvl) {
-  var toReturn = "";
-  for (let i = 0; i < lvl; i++) {
-    toReturn += " ";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
   }
-  return toReturn;
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var import_fs = require("fs");
+var readline = __toESM(require("readline"));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+var dir = "";
+rl.question("Please input the .pyc file you would like to convert (do not include the file extension): ", function(answer) {
+  dir = answer + ".pyc";
+  readPycFile(dir);
+  rl.close();
+});
+function readPycFile(file) {
+  const orig = (0, import_fs.readFileSync)(file, "utf8");
+  var lines = orig.replaceAll("function", "def").replaceAll("constructor(", "__init__(self,").replaceAll("this", "self").split("\n");
+  var converted = "";
+  String.prototype.isUpperCase = function() {
+    if (!/^[a-zA-Z]+$/.test(this.toString()))
+      return false;
+    return this.toString() === this.toUpperCase();
+  };
+  String.prototype.isLowerCase = function() {
+    if (!/^[a-zA-Z]+$/.test(this.toString()))
+      return false;
+    return this.toString() === this.toLowerCase();
+  };
+  String.prototype.isModifiable = function() {
+    return /^[a-zA-Z]+$/.test(this.toString());
+  };
+  var indentLevel = 0;
+  lines.forEach((l, i) => {
+    var stringSoFar = "";
+    var prev = "";
+    l.split("").forEach((c, i2) => {
+      const prev2 = l.split("")[i2 - 1];
+      if (/^\s+$/.test(c)) {
+        if (/^\s+$/.test(c + prev2))
+          return;
+      }
+      if (c == "}") {
+        indentLevel--;
+        return;
+      }
+      if (c == "{" && i2 == l.length - 1) {
+        indentLevel++;
+        stringSoFar += ":";
+        return;
+      }
+      if (i2 == 0)
+        stringSoFar = indent(indentLevel) + stringSoFar;
+      if (c.isUpperCase()) {
+        c = "_" + c.toLowerCase();
+      }
+      if (c !== ";")
+        stringSoFar += c;
+      if (stringSoFar == "var ")
+        stringSoFar = "";
+    });
+    if (!/^\s+$/.test(stringSoFar))
+      converted += stringSoFar + "\n";
+  });
+  console.log("Saved!");
+  (0, import_fs.writeFileSync)(file.replaceAll(".pyc", "") + "_converted.py", converted);
+  function indent(lvl) {
+    var toReturn = "";
+    for (let i = 0; i < lvl; i++) {
+      toReturn += " ";
+    }
+    return toReturn;
+  }
 }
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,60 +1,75 @@
 import { readFileSync as readFile, writeFileSync as saveFile } from 'fs';
+import * as readline from 'readline';
 
-const orig: string = readFile('test.pyc', 'utf8')
-var lines: Array<string> = orig.replaceAll('function', 'def').replaceAll('constructor(', '__init__(self,').replaceAll('this', 'self').split('\n')
-var converted: string = ''
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-//console.log(orig)
+var dir = ''
+rl.question("Please input the .pyc file you would like to convert (do not include the file extension): ", function(answer) {
+  dir = answer + '.pyc'
+  readPycFile(dir)
+  rl.close();
+});
 
-String.prototype.isUpperCase = function(): boolean {
-  if (!/^[a-zA-Z]+$/.test(this.toString())) return false;
-  return this.toString() === this.toUpperCase()
-}
-String.prototype.isLowerCase = function(): boolean {
-  if (!/^[a-zA-Z]+$/.test(this.toString())) return false;
-  return this.toString() === this.toLowerCase()
-}
-String.prototype.isModifiable = function(): boolean {
-  return /^[a-zA-Z]+$/.test(this.toString())
-}
+function readPycFile(file) {
+  const orig: string = readFile(file, 'utf8')
+  var lines: Array<string> = orig.replaceAll('function', 'def').replaceAll('constructor(', '__init__(self,').replaceAll('this', 'self').split('\n')
+  var converted: string = ''
 
-var indentLevel: number = 0
+  //console.log(orig)
 
-lines.forEach((l: string, i: number) => {
-  var stringSoFar: string = ''
-  var prev: string = ''
-  l.split('').forEach((c: string, i: number) => {
-    const prev = l.split('')[i - 1]
-    if (/^\s+$/.test(c)) {
-      if (/^\s+$/.test(c + prev)) return;
-    }
-    if (c == '}') {
-      indentLevel--
-      return;
-    }
-    if (c == '{' && i == l.length - 1) {
-      indentLevel++
-      stringSoFar += ':'
-      return;
-    }
-    if (i == 0) stringSoFar = indent(indentLevel) + stringSoFar
-    if (c.isUpperCase()) {
-      c = '_' + c.toLowerCase()
-    }
-    if (c !== ';') stringSoFar += c
-    if (stringSoFar == 'var ') stringSoFar = ''
-    //console.log(stringSoFar)
-  })
-  if (!/^\s+$/.test(stringSoFar)) converted += stringSoFar + '\n'
-})
-
-console.log(converted)
-saveFile('test_converted.py', converted)
-
-function indent(lvl: number): string {
-  var toReturn: string = ''
-  for (let i: number = 0; i < lvl; i++) {
-    toReturn += ' '
+  String.prototype.isUpperCase = function(): boolean {
+    if (!/^[a-zA-Z]+$/.test(this.toString())) return false;
+    return this.toString() === this.toUpperCase()
   }
-  return toReturn;
+  String.prototype.isLowerCase = function(): boolean {
+    if (!/^[a-zA-Z]+$/.test(this.toString())) return false;
+    return this.toString() === this.toLowerCase()
+  }
+  String.prototype.isModifiable = function(): boolean {
+    return /^[a-zA-Z]+$/.test(this.toString())
+  }
+
+  var indentLevel: number = 0
+
+  lines.forEach((l: string, i: number) => {
+    var stringSoFar: string = ''
+    var prev: string = ''
+    l.split('').forEach((c: string, i: number) => {
+      const prev = l.split('')[i - 1]
+      if (/^\s+$/.test(c)) {
+        if (/^\s+$/.test(c + prev)) return;
+      }
+      if (c == '}') {
+        indentLevel--
+        return;
+      }
+      if (c == '{' && i == l.length - 1) {
+        indentLevel++
+        stringSoFar += ':'
+        return;
+      }
+      if (i == 0) stringSoFar = indent(indentLevel) + stringSoFar
+      if (c.isUpperCase()) {
+        c = '_' + c.toLowerCase()
+      }
+      if (c !== ';') stringSoFar += c
+      if (stringSoFar == 'var ') stringSoFar = ''
+      //console.log(stringSoFar)
+    })
+    if (!/^\s+$/.test(stringSoFar)) converted += stringSoFar + '\n'
+  })
+
+  console.log('Saved!')
+  saveFile(file.replaceAll('.pyc','')+'_converted.py', converted)
+
+  function indent(lvl: number): string {
+    var toReturn: string = ''
+    for (let i: number = 0; i < lvl; i++) {
+      toReturn += ' '
+    }
+    return toReturn;
+  }
 }
